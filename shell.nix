eef43b785fe394bdec889c2546200d70f18dbb5f
Merlin Humml <megoettlinger@gmail.com>
2024-12-02 07:37:14 +0100

    Pinning nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ sources ? import nix/sources.nix, pkgs ? import sources.nixpkgs {} }:
 
 pkgs.mkShell {
   packages = with pkgs; [ ocaml ocamlPackages.findlib dune_3 ocamlPackages.menhir ocamlPackages.ocamlgraph ];
