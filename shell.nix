1674f60d3d1cbea94222227e986954594cef1f88
Merlin <megoettlinger@gmail.com>
2024-12-02 07:17:53 +0100

    Create CI workflow

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
-let pkgs = import <nixpkgs> {};
-in
+{ pkgs ? import <nixpkgs> {} }:
+
 pkgs.mkShell {
   packages = with pkgs; [ ocaml ocamlPackages.findlib dune_3 ocamlPackages.menhir ocamlPackages.ocamlgraph ];
 }
